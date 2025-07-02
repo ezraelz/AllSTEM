5255fa9ba0c4a5bb87668e32fe6c737f433bca11
Esrael Zerihun <144590580+ezraelz@users.noreply.github.com>
2025-07-01 21:56:34 -0700

    post

--- frontend/src/pages/auth/Authenticate.tsx
+++ frontend/src/pages/auth/Authenticate.tsx
@@ -6,31 +6,45 @@ import './Authenticate.css';
 import logo from '../../assets/logo/logopng.png';
 
 const Authenticate = () => {
-    const [loading, setLoading] = useState(false);
-    const [open, setOpen] = useState(false);
-    const navigate = useNavigate();
-    
-    const handleOpen = ()=> {
-      setOpen(!open);
-      setLoading(true);
-    }
+  const [activeForm, setActiveForm] = useState<'login' | 'register'>('login');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleClick = (form: 'login' | 'register') => {
+    setActiveForm(form);
+    setLoading(true);
+  };
 
   return (
     <div className='authenticate'>
       <div className="side-section">
-        <img src={logo} alt="" />
-        <h1>AllSTEM </h1>
+        <img src={logo} alt="AllSTEM Logo" />
+        <h1>AllSTEM</h1>
         <span>Fun and Education</span>
       </div>
+
       <div className="container">
         <div className="header">
-          <button title='login' onClick={handleOpen}>Login</button>
-          <button title='login' onClick={handleOpen}>Register</button>
+          <button
+            title='login'
+            onClick={() => handleClick('login')}
+            className={`button ${activeForm === 'login' ? 'active' : ''}`}
+          >
+            Login
+          </button>
+          <button
+            title='signup'
+            onClick={() => handleClick('register')}
+            className={`button ${activeForm === 'register' ? 'active' : ''}`}
+          >
+            Register
+          </button>
         </div>
-        {loading && open ? <Register/> : <Login/>}
+
+        {loading ? (activeForm === 'register' ? <Register /> : <Login />) : null}
       </div>
     </div>
   )
 }
 
-export default Authenticate
+export default Authenticate;
