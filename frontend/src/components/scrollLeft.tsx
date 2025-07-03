f09fba442595c5cb68abefb712c78e4d7e9a68ec
Esrael Zerihun <144590580+ezraelz@users.noreply.github.com>
2025-07-02 23:18:05 -0700

    people

---
+++ frontend/src/components/scrollLeft.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface ScrollProps{
+    isScrolled: ()=> void;
+    scrolled: boolean;
+}
+
+const ScrollLeft: React.FC<ScrollProps> = ({isScrolled, scrolled}) => {
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ScrollLeft
